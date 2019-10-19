1d43f305f7b5180b357bb705a1c9727dae58247f
Administrator <335075334@qq.com>
2019-10-19 12:19:43 +0800

    Merge branch 'master' of https://github.com/tan7seven/mall-parent into wjztest
    # Conflicts:
    #	mall-admin/src/main/java/com/mall/malladmin/test/fenzhi.java

--- mall-admin/src/main/java/com/mall/malladmin/test/fenzhi.java
+++ mall-admin/src/main/java/com/mall/malladmin/test/fenzhi.java
@@ -5,6 +5,7 @@ public class fenzhi {
     public String test(){
         String fenzhi = "主干合并后";
         String fenzhi2 = "主干更新";
+        String fenzhi3 = "fenzhi主干更新-添加";
         return "";
     }
 }
