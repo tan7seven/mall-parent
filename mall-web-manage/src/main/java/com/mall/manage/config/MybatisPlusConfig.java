2712757facaf08ebaf5aa02a5ec8d6528475e9d1
weijiazheng <weijiazheng@bisinuolan.com>
2020-03-07 17:54:06 +0800

    maven 依赖

---
+++ mall-web-manage/src/main/java/com/mall/manage/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package com.mall.manage.config;
+
+/**
+ * @author: huanggs
+ * @Date: 2018-12-26
+ * @Description:
+ */
+//@EnableTransactionManagement
+//@Configuration
+//@MapperScan("com.mall.dao.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        return new PaginationInterceptor();
+//    }
+//
+//    @Bean
+//    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+//        return new OptimisticLockerInterceptor();
+//    }
+}
--- mall-web-manage/src/main/java/com/mall/manage/config/WebConfig.java
+++ mall-web-manage/src/main/java/com/mall/manage/config/WebConfig.java
@@ -1,14 +1,24 @@
 package com.mall.manage.config;
 
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.MultipartConfigElement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -37,6 +47,34 @@ public class WebConfig implements WebMvcConfigurer {
         /// 设置总上传数据总大小
         factory.setMaxRequestSize("5MB");
         return factory.createMultipartConfig();
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(getMappingJackson2HttpMessageConverter(converters));
+    }
+    private MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter(List<HttpMessageConverter<?>> converters) {
+        List<MappingJackson2HttpMessageConverter> originalConverters = new ArrayList();
+        Iterator var2 = converters.iterator();
+        while(var2.hasNext()) {
+            HttpMessageConverter<?> converter = (HttpMessageConverter)var2.next();
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                originalConverters.add((MappingJackson2HttpMessageConverter)converter);
+            }
+        }
+        if (!CollectionUtils.isEmpty(originalConverters)) {
+            converters.removeAll(originalConverters);
+        }
+
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        return jackson2HttpMessageConverter;
     }
 
 }
