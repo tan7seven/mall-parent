448c87d64b9df9f0cf1dd1d03a71172650a3d80b
weijiazheng <weijiazheng@bisinuolan.com>
2020-04-07 09:57:26 +0800

    test

--- mall-web-manage/src/main/java/com/mall/manage/test/Instance.java
+++ mall-web-manage/src/main/java/com/mall/manage/test/Instance.java
@@ -7,7 +7,6 @@ public class Instance {
         System.out.println(person.toString());
         Person p1 = new Person();
         p1.setAge(11);
-        p1.setName("测试");
         p1.setProvince("222");
         Person p2 = p1.clone();
         System.out.println(p2.toString());
